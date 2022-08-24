c29946772c9a3f8efb2d36c4b6ba68eb26e6ab2a
Amos <amos2580@hotmail.co.uk>
2022-08-24 14:26:54 +0100

    Add examples for cfg

--- IW4M-LowPopMapRotate/ServerConfiguration.cs
+++ IW4M-LowPopMapRotate/ServerConfiguration.cs
@@ -4,10 +4,11 @@ namespace IW4MLowPopRotation;
 
 public class ServerConfiguration : IBaseConfiguration
 {
-    public List<long> ServersWithRotation { get; set; } = new();
+    public List<long> ServersWithRotation { get; set; } = new() {1111111111, 2222222222, 3333333333};
+
     public int CheckInMilliseconds { get; set; } = 60_000;
     public int Version { get; set; } = Plugin.ConfigurationVersion;
-    
+
     public string Name() => "IncludedServers";
     public IBaseConfiguration Generate() => new ServerConfiguration();
 }
