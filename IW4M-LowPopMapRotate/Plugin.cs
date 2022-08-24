9b5b593369f6035dabada4e1e70881c882a594e5
Amos <amos2580@hotmail.co.uk>
2022-08-24 12:28:42 +0100

    Init

---
+++ IW4M-LowPopMapRotate/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using SharedLibraryCore;
+using SharedLibraryCore.Interfaces;
+using Timer = System.Timers.Timer;
+
+namespace IW4M_LowPopMapRotate;
+
+public class Plugin : IPlugin
+{
+    public string Name => "IW4M Low Pop Map Rotate";
+    public float Version => 20220803;
+    public string Author => "Amos";
+    
+    private readonly ServerManager _serverManager = new();
+    
+    public Task OnEventAsync(GameEvent gameEvent, Server server)
+    {
+        _serverManager.EventUpdate(server);
+
+        return Task.CompletedTask;
+    }
+
+    public Task OnLoadAsync(IManager manager)
+    {
+        var timer = new Timer();
+        timer.Interval = 60_000;
+        timer.Elapsed += _serverManager.TimerTrigger;
+        timer.AutoReset = true;
+        timer.Enabled = true;
+
+        Console.WriteLine($"{Name} v{Version} by {Author} loaded");
+        return Task.CompletedTask;
+    }
+
+    public Task OnUnloadAsync()
+    {
+        Console.WriteLine($"{Name} unloaded");
+        return Task.CompletedTask;
+    }
+
+    public Task OnTickAsync(Server server)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ IW4M-LowPopMapRotate/ServerInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace IW4M_LowPopMapRotate;
+
+public class ServerInfo
+{
+    public int ClientNum { get; set; }
+    public string CurrentMapName { get; set; }
+}
